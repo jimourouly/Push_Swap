4fc76aa61aa629906b9bca8e2ec8c2465e8d8961
Jimmy Roulet <jroulet@c1r7s2.42lausanne.ch>
2024-02-17 21:52:52 +0100

    todo : av -> tab
    need to secure my tab full of av to free them properly

--- totab.c
+++ totab.c
@@ -6,11 +6,38 @@
 /*   By: jroulet <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/17 18:05:11 by jroulet           #+#    #+#             */
-/*   Updated: 2024/02/17 20:01:37 by jroulet          ###   ########.fr       */
+/*   Updated: 2024/02/17 21:51:24 by jroulet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	doublechecker(char **tab)
+{
+	ft_printf("DOUBLECHECKER\n");
+	int	i;
+	int	j;
+	int	len;
+
+	len = 0;
+	i = 0;
+	while (tab[len])
+		len ++;
+		
+	while (tab[i])
+	{
+		j = len - 1;
+		while (j > i)
+		{
+			//ft_printf("tab[%d] = %s tab[%d] = %s \n",i, tab[i], j, tab[j] );
+			if (ft_atoi(tab[i]) == ft_atoi(tab[j]))
+				return (0);
+			j--;
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	checker (char **tab)
 {
@@ -26,9 +53,10 @@ int	checker (char **tab)
 		j = 0;
 		while (tab[i][j])
 		{
+			if (tab[i][j] == '-' || tab[i][j] == '+')
+				j ++;
 			if (!ft_isdigit(tab[i][j]))
 			{
-				ft_printf("pas int");
 				return (0);
 			}
 			j++;
@@ -41,11 +69,11 @@ int	checker (char **tab)
 		digit = (ft_atoli(tab[i]));
 		if (digit < INT_MIN || digit > INT_MAX)
 		{
-			ft_printf("trop gros");
 			return (0);
 		}
 		i++;
 	}
+	tab[i] = NULL;
 	return (1);
 }
 
@@ -55,28 +83,36 @@ int	**tabint (int ac, char **av)
 	char	**tab;
 	int		i;
 	int		check;
-	int		**inttab;
 
-	if (ac == 2)
+	i = 1;
+	if (ac >= 2)
 	{
-		tab = ft_split(av[1], ' ');
-	}
-	else
-	{
-		i = 1;
-		tab = ft_calloc(ac + 1, sizeof(char *));
-		while (i < ac)
+		while (i <= ac)
 		{
-			tab[i - 1] = av[i];
+			tab = ft_split(av[1], ' ');
 			i++;
 		}
 	}
 	//to delete later
-	i = 0;
-	while (tab[i])
-		ft_printf("%s\n", tab[i++]);
+	i = 0 ;
+	while (i < ac)
+	{
+		ft_printf("%s\n", tab[i]);
+		i++;
+	}
 	//to delete later
 	check = checker(tab);
+	if (check == 0)
+	{
+		i --;
+		while (i >= 0)
+		{
+			ft_printf("tab[%d] %s part pour la liberte\n", i, tab[i]);
+			free(tab[i]);
+			i -- ;
+		}
+		free(tab);
+	}
 	ft_printf("is int = %d\n", check);
-	return (*tab);
+	return (NULL);
 }
